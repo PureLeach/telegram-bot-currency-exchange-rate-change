9cc848e99956db98b772b96bc4661f4c005a100b
barannikovMU <barannikov.mu@gmail.com>
2023-02-05 05:27:10 +0300

    minor fix

--- tests/test_handlers/test_exchange_rate.py
+++ tests/test_handlers/test_exchange_rate.py
@@ -13,6 +13,5 @@ async def test_send_current_exchange_rate(*args, **kwargs):
     message = AsyncMock()
     with patch('services.exchange_rate.collect_users_exchange_rates', return_value='data') as mock:
         await exchange_rate.send_current_exchange_rate(message)
-    
-    message.reply.assert_called_once_with('Current exchange rates:\n\n' + mock.return_value)
 
+    message.reply.assert_called_once_with('Current exchange rates:\n\n' + mock.return_value)
